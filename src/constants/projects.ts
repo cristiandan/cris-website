83f97476216e72192524acf53cb86812314e2be1
Cristian Dan <cristian.dan@live.com>
2026-02-27 21:50:36 +0000

    Yayzy: add video + video links (crowdfunding, promo, Antler)

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -53,7 +53,12 @@ export const projects: Project[] = [
       { title: "Product Hunt", url: "https://www.producthunt.com/products/yayzy" },
       { title: "PwC Future50 Report", url: "https://www.pwc.co.uk/issues/esg/pdf/net-zero-future-50-2022.pdf" },
       { title: "Apple App of the Day", url: "https://apps.apple.com/gb/story/id1539957783" },
+      { title: "Crowdfunding 2020", url: "https://youtu.be/zvyYckIAKE4" },
+      { title: "Crowdfunding 2022", url: "https://vimeo.com/720574853/2b891d3ac2" },
+      { title: "Antler Promo 2019", url: "https://www.youtube.com/watch?v=lODg7xSkmvw" },
+      { title: "Promo 2022", url: "https://vimeo.com/736152088" },
     ],
+    video: "https://youtu.be/zvyYckIAKE4",
     images: [
       "/images/projects/yayzy/founders-banner.jpg",
       "/images/projects/yayzy/team-event.jpg",
